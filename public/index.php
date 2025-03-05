236b7e8c0df97abb17b6ce01088dc66181301773
Mohammad Shoubash <mohammedshobash2002@gmail.com>
2025-03-05 23:22:19 +0300

    fix links

--- public/index.php
+++ public/index.php
@@ -5,25 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
     <script src="https://kit.fontawesome.com/d890c03bb3.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./assets/css/navbar.css">
-    <link rel="stylesheet" href="./assets/css/hero.css">
-    <link rel="stylesheet" href="./assets/css/featured.css">
-    <link rel="stylesheet" href="./assets/css/categories.css">
-    <link rel="stylesheet" href="./assets/css/about.css">
-    <link rel="stylesheet" href="./assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/hero.css">
+    <link rel="stylesheet" href="assets/css/featured.css">
+    <link rel="stylesheet" href="assets/css/categories.css">
+    <link rel="stylesheet" href="assets/css/about.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
-    <?php include 'navbar.html'; ?>
-    <?php include 'hero.html'; ?>
-    <?php include 'featured.html'; ?>
-    <?php include 'collections.html'; ?>
-    <?php include 'newArrival.html'; ?>
-    <?php include 'aboutSection.html'; ?>
-    <?php include 'footer.html'; ?>
+    <?php include 'views/components/navbar.html'; ?>
+    <?php include 'views/components/hero.html'; ?>
+    <?php include 'views/components/featured.html'; ?>
+    <?php include 'views/components/collections.html'; ?>
+    <?php include 'views/components/newArrival.html'; ?>
+    <?php include 'views/components/aboutSection.html'; ?>
+    <?php include 'views/components/footer.html'; ?>
 
-    <script src="./assets/js/categories.js"></script>
-    <script src="./assets/js/featured.js"></script>
-    <script src="./assets/js/hero.js"></script>
-    <script src="./assets/js/navbar.js"></script>
+    <script src="assets/js/categories.js"></script>
+    <script src="assets/js/featured.js"></script>
+    <script src="assets/js/hero.js"></script>
+    <script src="assets/js/navbar.js"></script>
 </body>
 </html>
